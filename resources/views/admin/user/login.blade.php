a6d0af7895bdffce0fa3068198e404114ad2575e
cuixiaolong <cuixiaolong@apsiration.com-cn>
2017-12-27 20:32:05 +0800

    修正login——register页面的title

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>英雄会--后台管理</title>
+    <title>KBD-知识分享后台管理</title>
     @include('admin.public.style')
 </head>
 <body>
--- resources/views/admin/user/register.blade.php
+++ resources/views/admin/user/register.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>英雄会后台注册</title>
+    <title>KBD-知识分享后台注册</title>
     @include('admin.public.style')
 </head>
 <body>
